2f034947c7f7d188336dd626d0c2456b6598a1ce
Ekansh Mittal <ekanshmittal22@gmail.com>
2026-07-03 22:28:28 +0530

    ok

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: './env'
+});
+
+connectDB()
+.then(() => {
+    console.log("Connected to database");
+    app.on("error",(err) =>{
+        console.log("err: ",err);
+        throw err;  
+    })
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err) => {
+    console.log("Failed to connect to database", err);
+})
