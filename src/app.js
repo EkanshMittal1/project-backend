3452f1609bab4d24f2eb3c9b30baa7750c86e9d8
Ekansh Mittal <ekanshmittal22@gmail.com>
2026-07-21 22:44:26 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ credentials: true
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(static("public"));
+app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
 export {app}
